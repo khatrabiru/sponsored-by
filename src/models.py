18a2e57b7fe2e7e4eea1ea1cd1159d39e26e6d08
khatrabiru <khatrabiru@gmail.com>
2022-09-25 19:17:57 +0100

    first commit

---
+++ src/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from .database import Base
+from sqlalchemy.orm import relationship
+
+
+class Event(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    image_url = Column(String)
+    description = Column(String)
+    short_description = Column(String)
+    location = Column(String)
+    date = Column(Date)
+    sponsors = List
+
+
+class Sponsor(Base):
+    __tablename__ = 'sponsors'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    image_url = Column(String)
+    description = Column(String)
+    short_description = Column(String)
+    headquarter_location = Column(String)
+    website_url = Column(String)
+    facebook_url = Column(String)
+    twitter_url = Column(String)
+    instagram_url = Column(String)
